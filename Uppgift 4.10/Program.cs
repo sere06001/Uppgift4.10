0d057fa3b7f417822df64ccae48bb7dca7f4b923
sere06001 <sere06001@utb.vaxjo.se>
2023-10-19 12:51:54 +0200

    Add project files.

---
+++ Uppgift 4.10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Uppgift4_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många X per grupp?");
+            int antalX = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många O per grupp?");
+            int antalO = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många O mönster per rad?");
+            int antalOMönster = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många rader?");
+            int antalRader = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < antalRader; i++)
+            {
+                string utskrivnaX = new string('X', antalX);
+                string utskrivnaO = new string('O', antalO);
+                string utskrivet = utskrivnaX + "-" + utskrivnaO + "-";
+                string utskrivnaOMönster = string.Concat(Enumerable.Repeat(utskrivet, antalOMönster));
+                Console.WriteLine($"{utskrivnaOMönster}{utskrivnaX}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
